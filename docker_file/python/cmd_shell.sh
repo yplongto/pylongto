7eef6cb708e2200afc646efb9ca19cc63ec0f97a
yplongto <15558703665@163.com>
2025-04-15 09:29:48 +0800

    添加图片文件

--- docker_file/python/cmd_shell.sh
+++ docker_file/python/cmd_shell.sh
@@ -5,14 +5,23 @@ pip freeze > requirements.txt
 # 2. 创建Dockerfile
 
 # 3. 构建Docker镜像
-docker build -t  .
+docker build -t python3.12.9 .
 
-# 4. 运行Python环境容器 基本运行方式 这会启动一个交互式Python shell
-docker run -it --rm my-python-env
-# 带卷挂载的运行方式（适合开发）这样可以将主机当前目录挂载到容器的/app目录
-docker run -it --rm -v $(pwd):/app my-python-env
-# 带端口映射的运行方式（如运行web应用）
-docker run -it --rm -p 8000:8000 my-python-env
+# 启动并挂载目录
+docker run -v /宿主机路径:/容器路径 python:3.12.9
+docker run -d --name python-app -v /opt/pylongto/app:/usr/src/backend python:3.12.9
+# 挂载单个文件
+docker run -v /宿主机文件:/容器文件 python:3.12.9
+docker run -it --name python-app -v /opt/pylongto/app/config.json:/usr/src/backend/config.json python:3.12.9 bash
+# 只读挂载	docker run --mount type=bind,source=...,target=...,readonly python:3.12.9
+docker run -d \
+  --name python-app \
+  --mount type=bind,source=/opt/pylongto/app,target=/usr/src/backend,readonly \
+  python:3.12.9
+端口映射	docker run -p 8000:8000 python:3.12.9
+
+#进入容器bash
+docker exec -it python-app bash
 
 ----------------- 使用docker-compose管理 ---------------------
 # 创建docker-compose.yml文件
